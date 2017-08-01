4350cee4e8b6fc40377a22d3274af2febc6bd9eb
ryazanova.ea <ryazanova.ea@igit.spb.ru>
2017-08-01 19:01:21 +0300

    Initial commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by ryazanova.ea on 31.07.2017.
+ */
+
+function onChangeFIO(value) {
+  let array = value.split(' ')
+  if (array.length!==3){
+
+  }
+  console.log('dfgdfg', value)
+}
+
+function onChangeEmail(value) {
+  console.log('dfgdfg', value)
+}
+
+function onChangePhone(value) {
+  console.log('dfgdfg', value)
+}
